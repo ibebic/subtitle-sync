98fd7ae794282cd3f272ae131d9ce28ee1d4b909
igor.bebic@gmail.com <igor.bebic@gmail.com>
2016-09-23 16:39:51 +0200

    Using read-write streams

--- cli.js
+++ cli.js
@@ -1,22 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 const fs = require('fs');
+const es = require('event-stream');
 const helpers = require('./helpers.js');
 const timeSync = helpers.timeSync;
 let args = process.argv.slice(2);
-// reading the .srt file and storing it in a variable srt
-let srt = fs.readFileSync('./sampleFile.srt', 'utf8', function (err, data) {
-  if (err) {
-    return console.log('.srt file not found');
-  }
-  return data;
-});
-// replace all timestamps from srt with it's corresponding ajusted timestamp
-srt = srt.replace(/\d{2}:\d{2}:\d{2},\d{3}/g, timeStamp => timeSync(timeStamp, args));
-// write the modified srt to a new file
-fs.writeFile('./convFile.srt', srt, function (err) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log('Modified .srt created!');
-});
+
+fs.createReadStream('sampleFile.srt')  // process.stdin
+  .pipe(es.split())
+  .pipe(es.mapSync(line =>
+    line.replace(/\d{2}:\d{2}:\d{2},\d{3}/g, timeStamp => timeSync(timeStamp, args))))
+  .pipe(es.join('\n'))
+  .on('error', err => console.error(err))
+  .on('end', () => console.error('Modified .srt created!'))
+  .pipe(fs.createWriteStream('convFile.srt')); // .pipe(process.stdout);
